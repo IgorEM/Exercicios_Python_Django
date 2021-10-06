5bb904e5ab5677c1a20dc5e41d43aed0b65bc5be
IgorEM <iem@cin.ufpe.br>
2021-10-06 08:21:16 -0300

    deletado arquivos do desafio 1

--- Desafio Técnico da Semana 1/settings.py
+++
@@ -1,5 +0,0 @@
-from decouple import config
-
-APIKEY = config("APIKEY")
-BASE_URL = config("BASE_URL", "https://www.alphavantage.co/query")
-#https://www.alphavantage.co/query?function=TIME_SERIES_INTRADAY&symbol=IBM&interval=5min&apikey=demo
