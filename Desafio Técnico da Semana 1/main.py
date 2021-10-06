8974ab08c3e5f69942238cb49f80b98785810c6f
IgorEM <iem@cin.ufpe.br>
2021-10-06 13:50:57 -0300

    adicionado intraday_series

--- Desafio Técnico da Semana 1/main.py
+++ Desafio Técnico da Semana 1/main.py
@@ -2,6 +2,12 @@ import json
 import settings
 import requests
 
+#https://www.alphavantage.co/query?function=TIME_SERIES_INTRADAY&symbol=IBM&interval=5min&apikey=demo
+# **kwargs - numero ilimitado de argumentos. vamos simular um dicionario
+class APICallError(Exception):
+    pass
+
+
 class StockTimeSeries:
     def __init__(self):
         self.base_url = settings.BASE_URL
@@ -12,9 +18,39 @@ class StockTimeSeries:
         return f"{self.base_url}?{path}&apikey={self.api_key}"
 
 
-    def Intraday(self,symbol,interval):
+    def intraday_series(self,function,symbol,interval, **kwargs):
+        #function = TIME_SERIES_INTRADAY & symbol = IBM & interval = 5min
+        #lista = []
         if interval == '1min' or '5min' or '15min' or '30min' or '60min':
-            path = "function="
-            #https: // www.alphavantage.co / query?function = TIME_SERIES_INTRADAY & symbol = IBM & interval = 5min & apikey = demo
-# sts = StockTimeSeries()
-# print(sts)
+            path = f"function={function}&symbol={symbol}&interval={interval}"
+            options = [f"{item[0]}={item[1]}" for item in kwargs.items()]
+            path = f"{path}&{'&'.join(options)}" if options else path
+            # for c, v in kwargs.items():
+            #     lista.append(f"{c}={v}")
+            # if lista:
+            #     path = f"{path}&{'&'.join(lista)}"
+            # else:
+            #     path
+        else:
+            print("Intervalo Inválido")
+
+        url = self._build_url(path)
+
+        resp = requests.get(url)
+
+        if resp.status_code == 200:
+            return resp.json()
+        raise APICallError(f"Não foi possivel consumir o serviço: {url}")
+
+        #return path
+
+
+
+
+
+obj_sts = StockTimeSeries()
+dados = obj_sts.intraday_series("TIME_SERIES_INTRADAY","IBM","5min")
+print(dados)
+
+
+
