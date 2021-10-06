75ab5ef600bc9618edcdf091ef08a577b4d1c424
IgorEM <iem@cin.ufpe.br>
2021-10-06 13:51:56 -0300

    adicionado exemplos de uso

--- Desafio Técnico da Semana 1/brincando_testes.py
+++ Desafio Técnico da Semana 1/brincando_testes.py
@@ -1,6 +1,9 @@
-import requests
 import json
+import settings
+import requests
+from main import StockTimeSeries
 
+# testando requests
 url= ("https://www.alphavantage.co/query?"
       "function=TIME_SERIES_INTRADAY"
       "&symbol=IBM"
@@ -15,3 +18,19 @@ print(f"data :{data}")
 with open("data.json","w") as fp:
      fp.write(json.dumps(data, indent=4))
 
+#testando class
+obj_sts = StockTimeSeries()
+path = obj_sts.intraday_series("TIME_SERIES_INTRADAY","IBM","5min")
+print(path)
+print(obj_sts._build_url(path))
+
+#testando problema do **kwargs
+d = {'a': 1, 'b': 2}
+l = [f"{k[0]}={k[1]}" for k in d.items()]
+
+lista = []
+
+for c, v in d.items():
+    lista.append(f"{c}={v}")
+
+print(lista)
