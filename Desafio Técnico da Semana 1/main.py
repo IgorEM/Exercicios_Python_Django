93fcacdfcf76dcf2d32627cce1b74d3eddb1f3d4
IgorEM <iem@cin.ufpe.br>
2021-10-06 22:01:54 -0300

    add def TratandoKwargs, class IntervalInvalid

--- Desafio Técnico da Semana 1/main.py
+++ Desafio Técnico da Semana 1/main.py
@@ -3,10 +3,12 @@ import settings
 import requests
 
 #https://www.alphavantage.co/query?function=TIME_SERIES_INTRADAY&symbol=IBM&interval=5min&apikey=demo
-# **kwargs - numero ilimitado de argumentos. vamos simular um dicionario
+# **kwargs - numero ilimitado de argumentos. adjusted="false",outputsize="compact"
 class APICallError(Exception):
     pass
 
+class IntervalInvalid(Exception):
+    pass
 
 class StockTimeSeries:
     def __init__(self):
@@ -17,22 +19,25 @@ class StockTimeSeries:
     def _build_url(self,path):
         return f"{self.base_url}?{path}&apikey={self.api_key}"
 
+    def TratandoKwargs(self, kwargs, path):
+        lista = []
+        for c, v in kwargs.items():
+            lista.append(f"{c}={v}")
+        if lista:
+            path = f"{path}&{'&'.join(lista)}"
+        else:
+            path
+
 
     def intraday_series(self,function,symbol,interval, **kwargs):
         #function = TIME_SERIES_INTRADAY & symbol = IBM & interval = 5min
-        #lista = []
-        if interval == '1min' or '5min' or '15min' or '30min' or '60min':
+        if interval in ("1min", "5min", "15min", "30min", "60min"):
             path = f"function={function}&symbol={symbol}&interval={interval}"
-            options = [f"{item[0]}={item[1]}" for item in kwargs.items()]
-            path = f"{path}&{'&'.join(options)}" if options else path
-            # for c, v in kwargs.items():
-            #     lista.append(f"{c}={v}")
-            # if lista:
-            #     path = f"{path}&{'&'.join(lista)}"
-            # else:
-            #     path
+            self.TratandoKwargs(kwargs, path)
         else:
             print("Intervalo Inválido")
+            raise IntervalInvalid("Intervalo invalido. Os intervalos válidos são: 1min,5min,15min,30min,60min")
+
 
         url = self._build_url(path)
 
@@ -42,14 +47,8 @@ class StockTimeSeries:
             return resp.json()
         raise APICallError(f"Não foi possivel consumir o serviço: {url}")
 
-        #return path
-
-
-
-
-
 obj_sts = StockTimeSeries()
-dados = obj_sts.intraday_series("TIME_SERIES_INTRADAY","IBM","5min")
+dados = obj_sts.intraday_series("TIME_SERIES_INTRADAY", "IBM", "60min", adjusted="false", outputsize="compact")
 print(dados)
 
 
