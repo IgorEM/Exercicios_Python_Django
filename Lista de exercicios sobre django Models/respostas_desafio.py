c428c0394b220905fe501f8e48d4b1065fb8b133
IgorEM <iem@cin.ufpe.br>
2021-10-16 09:23:42 -0300

    adicionando arquivo que vao ficar as respostas do desafio

---
+++ Lista de exercicios sobre django Models/respostas_desafio.py
@@ -0,0 +1,9 @@
+# Usando a API de modelos do Django, elabore queries para atender as seguintes operações
+#
+# 1. Uma lisa das n pessoas mais jovens, ordenadas ascendentemente
+# 2.Uma lista das n pessoas mais velhas ordenadas ascendentemente
+# 3.Qual a quantidade de pessoas do sexo feminino?
+# 4.Qual a quantidade de pessoas do sexo masculino?
+# 5.Escreva uma método "to_json", que retorne os dados de uma pessoa em formato json
+# 6.Listar os nomes de todas as pessoas no dataset em ordem alfabética
+# 7.Implemente uma função para buscar pessoas por nome, ou parte do nome (Case insensitive)
