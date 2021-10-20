be68cc670e33ab63cde35b62c3ba8f1a94e34acd
Igor Eduardo Mascarenhas <iem@cin.ufpe.br>
2021-10-20 18:07:00 -0300

    criada modelagem

--- Projeto Final/cartoriosBR/core/models.py
+++ Projeto Final/cartoriosBR/core/models.py
@@ -1,3 +1,65 @@
 from django.db import models
 
+
 # Create your models here.
+class Cartorios(models.Model):
+    cnpj = models.CharField(max_length=18)
+    cns = models.CharField(max_length=8)
+    data_de_instalacao = models.CharField(max_length=10)
+    nome_oficial = models.CharField(max_length=300)
+    nome_fantasia = models.CharField(max_length=300, null=True, blank=True)
+    nome_do_titular = models.CharField(max_length=100)
+    nome_do_substituto = models.CharField(max_length=100)
+    nome_do_juiz = models.CharField(max_length=100)
+    observacao = models.CharField(max_length=300)
+    ultima_atualizacao = models.CharField(max_length=300)
+    horario_de_funcionamento = models.CharField(max_length=300)
+    area_de_abrangencia = models.CharField(max_length=300)
+    atribuicoes = models.CharField(max_length=300)
+    comarca = models.CharField(max_length=300)
+    entrancia = models.CharField(max_length=300)
+
+
+class Adressess(models.Model):
+    UF_CHOICES = (
+        ('AC', 'Acre'),
+        ('AL', 'Alagoas'),
+        ('AP', 'Amapá'),
+        ('BA', 'Bahia'),
+        ('CE', 'Ceará'),
+        ('DF', 'Distrito Federal'),
+        ('ES', 'Espírito Santo'),
+        ('GO', 'Goiás'),
+        ('MA', 'Maranão'),
+        ('MG', 'Minas Gerais'),
+        ('MS', 'Mato Grosso do Sul'),
+        ('MT', 'Mato Grosso'),
+        ('PA', 'Pará'),
+        ('PB', 'Paraíba'),
+        ('PE', 'Pernanbuco'),
+        ('PI', 'Piauí'),
+        ('PR', 'Paraná'),
+        ('RJ', 'Rio de Janeiro'),
+        ('RN', 'Rio Grande do Norte'),
+        ('RO', 'Rondônia'),
+        ('RR', 'Roraima'),
+        ('RS', 'Rio Grande do Sul'),
+        ('SC', 'Santa Catarina'),
+        ('SE', 'Sergipe'),
+        ('SP', 'São Paulo'),
+        ('TO', 'Tocantins')
+    )
+
+    endereco = models.CharField(max_length=150)
+    bairro = models.CharField(max_length=100)
+    municipio = models.CharField(max_length=100)
+    cep = models.CharField(max_length=8, null=True)
+    uf = models.CharField(max_length=2, choices=UF_CHOICES)
+
+
+
+class ContactInformation(models.Model):
+    homepage = models.CharField(max_length=100)
+    email = models.CharField(max_length=100)
+    telefone = models.CharField(max_length=17)
+    fax = models.CharField(max_length=100)
