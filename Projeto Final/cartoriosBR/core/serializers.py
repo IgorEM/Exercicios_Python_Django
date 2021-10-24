43b90403b76cdb8beb188b52c4b312a138cf5da1
Igor Eduardo Mascarenhas <iem@cin.ufpe.br>
2021-10-24 00:14:54 -0300

    configurando Serializers

---
+++ Projeto Final/cartoriosBR/core/serializers.py
@@ -0,0 +1,25 @@
+from core.models import Cartorios
+from rest_framework import serializers
+
+
+class CartoriosSerializer(serializers.HyperlinkedModelSerializer):
+    class Meta:
+        model = Cartorios
+        fields = [
+                'cnpj',
+                'cns',
+                'data_de_instalacao',
+                'nome_oficial',
+                'nome_fantasia',
+                'nome_do_titular',
+                'nome_do_substituto',
+                'nome_do_juiz',
+                'observacao',
+                'ultima_atualizacao',
+                'horario_de_funcionamento',
+                'area_de_abrangencia',
+                'atribuicoes',
+                'comarca',
+                'entrancia',
+                'uf']
+
--- Projeto Final/cartoriosBR/core/views.py
+++ Projeto Final/cartoriosBR/core/views.py
@@ -1,3 +1,30 @@
-from django.shortcuts import render
+from core.models import Cartorios
+from rest_framework import viewsets
+from rest_framework import permissions
+from core.serializers import *
+from django.shortcuts import render, HttpResponse, redirect
 
+class CartoriosViewSet(viewsets.ModelViewSet):
+    """
+    Retorna lista de cartórios paginadas de 50 em 50
+    """
+    queryset = Cartorios.objects.all()
+    serializer_class = CartoriosSerializer
+    permission_classes = [permissions.IsAuthenticated]
 # Create your views here.
+
+# def retorna_uf(request,text_uf):
+#     uf = Cartorios.objects.get(uf=text_uf)
+#     return uf
+
+
+# class CartoriosUfViewSet(request,viewsets.ModelViewSet):
+#     """
+#     Retorna a lista de cartórios da UF paginadas de 50 em 50
+#     """
+#     uf = self.object.uf
+#     self.object.timein = self.request.POST.get("uf", "")
+#
+#     queryset = Cartorios.objects.all().filter(uf=uf)
+#     serializer_class = CartoriosSerializer
+#     permission_classes = [permissions.IsAuthenticated]
